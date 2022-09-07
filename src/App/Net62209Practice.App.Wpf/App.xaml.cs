14f2a6c486cdc34154cf65276bddbbca3eb934c4
Barefoot <shlomi_o@hotmail.com>
2022-09-07 18:38:06 +0300

    WIP - building the framework skeleton...

---
+++ src/App/Net62209Practice.App.Wpf/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Windows;
+
+namespace Net62209Practice.App.Wpf
+{
+    public partial class App : Application
+    {
+        private void Bootstraper(object sender, StartupEventArgs args)
+        {
+            using IHost host = CreateHostBuilder(args).Build();
+
+            new MainWindow().Show();
+        }
+
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureServices((_, services) =>
+            services.AddTransient<TransientDisposable>()
+                    .AddScoped<ScopedDisposable>()
+                    .AddSingleton<SingletonDisposable>());
+
+    }
+}
