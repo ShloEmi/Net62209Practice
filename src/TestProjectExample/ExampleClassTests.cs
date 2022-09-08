32baf070d0cffaa42881cb5b839c5f96f9c92504
Barefoot <shlomi_o@hotmail.com>
2022-09-08 12:09:19 +0300

    Self CR imp'v

--- src/TestProjectExample/ExampleClassTests.cs
+++ src/TestProjectExample/ExampleClassTests.cs
@@ -5,6 +5,10 @@ namespace TestProjectExample
 {
     public class ExampleClassTests
     {
+        /* TODO: This is an example development leftover */
+        // REMARK: This is an example remark leftover
+
+
         [Test, Ignore("Because ignored")]
         public void Test__TestIgnored__Ignore__ExpectedTestNotRunning() => 
             false.Should().Be(true);
