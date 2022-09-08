d888f8e28f5107f373d45ff2258f79475b5c19b0
Barefoot <shlomi_o@hotmail.com>
2022-09-08 12:04:18 +0300

    Tests skeleton

---
+++ src/TestProjectExample/UnitTest1.cs
@@ -0,0 +1,18 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TestProjectExample
+{
+    public class SpecificClassTests
+    {
+        [Test, Ignore("Because ignored")]
+        public void Test__TestIgnored__Ignore__ExpectedTestNotRunning() => 
+            false.Should().Be(true);
+
+        [Test]
+        public void Test__TestPass__Nothing__ExpectedPass()
+        {
+            true.Should().Be(true);
+        }
+    }
+}
