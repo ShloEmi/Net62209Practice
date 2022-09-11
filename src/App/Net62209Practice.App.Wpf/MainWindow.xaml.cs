ef1503e1d4a7215532a57980928e128af637933d
Barefoot <shlomi_o@hotmail.com>
2022-09-11 11:09:53 +0300

    + Window-title-Commands

--- src/App/Net62209Practice.App.Wpf/MainWindow.xaml.cs
+++ src/App/Net62209Practice.App.Wpf/MainWindow.xaml.cs
@@ -1,7 +1,21 @@
-﻿namespace Net62209Practice.App.Wpf;
+﻿using System.Windows;
+
+namespace Net62209Practice.App.Wpf;
 
 public partial class MainWindow
 {
     public MainWindow() => 
         InitializeComponent();
+
+
+    private void LaunchGitHubSite(object sender, RoutedEventArgs e)
+    {
+        // Launch the GitHub site...
+    }
+
+    private void DeployCupCakes(object sender, RoutedEventArgs e)
+    {
+        // deploy some CupCakes...
+    }
+
 }
