b49c907700ad18168a56d0cc59929a8893a1f088
Barefoot <shlomi_o@hotmail.com>
2022-09-11 17:23:31 +0300

    Self CR

--- src/Net62209Practice.Data.ApplicationData/ItemData.cs
+++ src/Net62209Practice.Data.ApplicationData/ItemData.cs
@@ -1,5 +1,4 @@
-﻿namespace Net62209Practice.Data.ApplicationData
-{
-    /// <example>     ItemData ItemData2 = ItemData1 with { Name = "John" }; </example>
-    public record ItemData(Guid Id, string Name, string Description, ItemCategorizationData ItemCategorization);
-}
+﻿namespace Net62209Practice.Data.ApplicationData;
+
+/// <example>     ItemData ItemData2 = ItemData1 with { Name = "John" }; </example>
+public record ItemData(Guid Id, string Name, string Description, ItemCategorizationData ItemCategorization);
