e224d33974f7697c7588dadc630a5ee6e8726335
Barefoot <shlomi_o@hotmail.com>
2022-10-07 15:19:34 +0300

    + OnItemsChanged done, presenting using DataGrid

--- src/App/Net62209Practice.App.Wpf/ViewModels/MainWindowViewModel.cs
+++ src/App/Net62209Practice.App.Wpf/ViewModels/MainWindowViewModel.cs
@@ -9,26 +9,23 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Reactive.Concurrency;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
-using System.Windows.Threading;
 
 namespace NoNameCompany.IMS.App.Wpf.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase, IStartable, IDisposable
 {
+    // ReSharper disable once CollectionNeverQueried.Local
     private readonly CompositeDisposable disposables = new();
 
     private readonly IDAL dataAccessLayer;
     private readonly ILogger logger;
     private string themeSelectedItem;
     private ObservableCollection<string> availableThemes = new();
-    private readonly ObservableCollection<ItemContainerData> itemContainerData = new();
-    
+
 
     public MainWindowViewModel(IDAL dataAccessLayer, ILogger logger, Faker<ItemData> itemDataProvider)
     {
@@ -62,90 +59,51 @@ public class MainWindowViewModel : ViewModelBase, IStartable, IDisposable
     public void Dispose() => 
         disposables.Clear();
 
-
     
     /// <remarks>If all is ok, will continue with <see cref="OnItemsChanged"/></remarks>
     private void ExecuteAddItemsCommand(Faker<ItemData> itemDataProvider, object? count)
     {
         int howMuch = int.Parse(count?.ToString() ?? "0");
-        bool result = dataAccessLayer.AddItemsBulk(
+        bool executed = dataAccessLayer.AddItemsBulk(
             Enumerable
                 .Range(1, howMuch)
                 .Select(_ => itemDataProvider.Generate()).ToArray()
         );
+
+        if (!executed) 
+            logger.Warning("Couldn't dataAccessLayer.AddItemsBulk for ...");
     }
 
-    private void OnItemsChanged(IEnumerable<ItemChanged> datum)
+    private void OnItemsChanged(IEnumerable<ItemChanged> changedItems)
     {
-#if false
-
-        foreach (ItemChanged itemChanged in datum)
+        foreach (ItemChanged itemChanged in changedItems)
         {
-            Dispatcher.CurrentDispatcher.InvokeAsync(
-                /*async*/ () =>
-                {
-                    switch (itemChanged.ChangeDescription)
-                    {
-                        case ChangeDescriptions.added:
-                            OnItemAdded(itemChanged);
-                            break;
-                        case ChangeDescriptions.removed:
-                            OnItemRemoved(itemChanged);
-                            break;
-                        case ChangeDescriptions.updated:
-                            OnItemUpdated(itemChanged);
-                            break;
-                        default:
-                            throw new ArgumentOutOfRangeException();
-                    }
-                });
-        }
-
-#else
-
-        Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Inactive, 
-            () =>
+            try
             {
-                //if(lockedObject.Contains("Items");
-                // lockedObject.Add("Items");
-                // {...}
-                // lockedObject.Remove("Items");
-
-                foreach (ItemChanged itemChanged in datum)
+                switch (itemChanged.ChangeDescription)
                 {
-                    switch (itemChanged.ChangeDescription)
-                    {
-                        case ChangeDescriptions.added:
-                            OnItemAdded(itemChanged);
-                            break;
-                        case ChangeDescriptions.removed:
-                            OnItemRemoved(itemChanged);
-                            break;
-                        case ChangeDescriptions.updated:
-                            OnItemUpdated(itemChanged);
-                            break;
-                        default:
-                            throw new ArgumentOutOfRangeException();
-                    }
+                    case ChangeDescriptions.added:
+                        ItemsDataSource.Add(itemChanged.ChangedItem);
+                        break;
+                    case ChangeDescriptions.removed:
+                        ItemsDataSource.Remove(itemChanged.ChangedItem);
+                        break;
+                    case ChangeDescriptions.updated:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
                 }
-
-            });
-#endif
-    }
-
-    private void OnItemUpdated(ItemChanged itemChanged)
-    {
-        Task.Delay(30000).Wait(); /* TODO: Shlomi, changing this will take ~1 RL month */
-    }
-
-    private void OnItemRemoved(ItemChanged itemChanged)
-    {
-        Task.Delay(1000).Wait();
-    }
-
-    private void OnItemAdded(ItemChanged itemChanged)
-    {
-        Task.Delay(1000).Wait();
+            }
+            catch (Exception exception)
+            {
+                logger.Warning(exception, "Couldn't change ItemsDataSource");
+            }
+            finally
+            {
+                base.OnPropertyChanged(
+                    nameof(ItemsDataSource)); /* TODO: Shlomi, this can be more easily done using Dynamic-Data: https://github.com/reactivemarbles/DynamicData#dynamic-data */
+            }
+        }
     }
 
 
@@ -164,6 +122,9 @@ public class MainWindowViewModel : ViewModelBase, IStartable, IDisposable
         get => availableThemes;
         set => SetField(ref availableThemes, value);
     }
+
+
+    public ObservableCollection<ItemData> ItemsDataSource { get; } = new();
 
     public ICommand AddItemsCommand { get; }
 }
